9298ce04d7865fd94eb23c5cc0cc022411ed6586
mverzi <maggie.verzi@gmail.com>
2022-11-13 12:41:05 -0500

    First commit

---
+++ sumTheStrings.js
@@ -0,0 +1,9 @@
+/**
+ * This function takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+ * @param {string} a 
+ * @param {string} b 
+ * @returns The sum of the strings as a string
+ */
+function sumStr(a,b) {
+    return Number(a) + Number(b) + ""
+  }
